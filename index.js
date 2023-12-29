f593e6e2b2565a86a0492686e50ea07e92d3305c
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2023-12-29 20:54:37 +0530

    Almost there

---
+++ index.js
@@ -0,0 +1,11 @@
+function change_mountain(mountain){
+    // I didn't have time to write these functions
+    // What I would do is use the visible and invisible class to hide all current sections
+    // Then I would use the passed argument to add the visible and relevant css tags to their respective elements.
+}
+
+function change_carousel(slide_number){
+    // I didn't have time to write these functions
+    // What I would do is use the visible and invisible class to hide all current sections
+    // Then I would use the passed argument to add the visible and relevant css tags to their respective elements.
+}
